6f109cf1b4c4f9fae54616ad16e908824e9abba7
Frankley Francalino da Rocha <rocha112358@gmail.com>
2025-02-12 07:49:05 -0300

    iniciando

---
+++ src/main/java/digital/ikr/groqapi/GroqApi.java
@@ -0,0 +1,78 @@
+package digital.ikr.groqapi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class GroqApi {
+
+    private final String baseUrl;
+    private final String apiKey;
+
+    public GroqApi(String apiKey){
+        this.baseUrl = "https://api.groq.com";
+        this.apiKey = apiKey;
+    }
+
+    public GroqApi(String baseUrl, String apiKey) {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+    }
+
+    private String apiCall(String endpointUrl, String requestBody){
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpointUrl))
+                .header("Authorization", "Bearer " + this.apiKey)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 200)
+                return response.body();
+            else
+                throw new RuntimeException("Error! response.status: "+response.statusCode()+", response.body: "+response.body());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public String completions(String model, List<Message> messages){
+
+        String endpointUrl = this.baseUrl+"/openai/v1/chat/completions";
+        String requestBody = "{\"model\":\""+model+"\", \"messages\":"+messages+"}";
+        String responseBody = apiCall(endpointUrl, requestBody);
+        String content = JsonPath.read(responseBody, "$.choices[0].message.content");
+
+        return content;
+
+    }
+
+    public record Message(String role, String content){
+
+        public final String toString() {
+
+            String jsonSafe = content
+                .replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
+
+            return "{\"role\":\""+role+"\",\"content\":\""+jsonSafe+"\"}";
+        }
+
+    }
+
+}
---
+++ src/main/java/digital/ikr/groqapi/Main.java
@@ -0,0 +1,57 @@
+package digital.ikr.groqapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String apiKey = System.getenv("GROQ_API_KEY");
+
+        GroqApi api = new GroqApi(apiKey);
+
+        List<GroqApi.Message> messages = new ArrayList<>();
+        messages.add(new GroqApi.Message(
+            "system",
+            "Você é um assistentente que fala português"
+        ));
+
+        Scanner sc = new Scanner(System.in);
+
+        String model = "llama-3.3-70b-versatile";
+
+        System.out.println("Bem vindo, você está conversando com o modelo: "+model);
+        System.out.println("Escreva \"sair\" para encerrar!");
+
+        while(true){
+
+            String userMessage = sc.nextLine();
+
+            if("sair".equals(userMessage.toLowerCase())){
+                sc.close();
+                break;
+            }
+
+            messages.add(new GroqApi.Message(
+                "user",
+                userMessage
+            ));
+
+            String assistantMessage = api.completions(
+                model,
+                messages
+            );
+
+            System.out.println(assistantMessage);
+
+            messages.add(new GroqApi.Message(
+                "assistant",
+                assistantMessage
+            ));
+
+        }
+
+    }
+}
