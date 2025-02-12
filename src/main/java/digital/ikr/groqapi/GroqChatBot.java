7089c58d06e55730962af038711139d183016433
Frankley Francalino da Rocha <rocha112358@gmail.com>
2025-02-12 09:51:37 -0300

    Implementação da classe GroqChatBot com memória!

---
+++ src/main/java/digital/ikr/groqapi/GroqChatBot.java
@@ -0,0 +1,44 @@
+package digital.ikr.groqapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroqChatBot {
+
+    private String model;
+    private GroqApi api;
+    private List<GroqApi.Message> memory;
+
+    public GroqChatBot(GroqApi api, String model) {
+        this.model = model;
+        this.api = api;
+    }
+
+    public GroqChatBot(GroqApi api, String model, String systemMessage) {
+        this.model = model;
+        this.api = api;
+        this.memory = new ArrayList<>();
+        this.memory.add(new GroqApi.Message("system",systemMessage));
+    }
+
+    public String completions(String message){
+
+        memory.add(new GroqApi.Message(
+            "user",
+            message
+        ));
+
+        String assistantMessage = api.completions(
+            model,
+            memory
+        );
+
+        memory.add(new GroqApi.Message(
+            "assistant",
+            assistantMessage
+        ));
+
+        return assistantMessage;
+    }
+
+}
--- src/main/java/digital/ikr/groqapi/examples/ChatBotExample.java
+++ src/main/java/digital/ikr/groqapi/examples/ChatBotExample.java
@@ -1,28 +1,24 @@
 package digital.ikr.groqapi.examples;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import digital.ikr.groqapi.GroqApi;
+import digital.ikr.groqapi.GroqChatBot;
 
 public class ChatBotExample {
 
     public static void main(String[] args) throws Exception {
 
         String apiKey = System.getenv("GROQ_API_KEY");
-
+        String model = "llama-3.3-70b-versatile";
         GroqApi api = new GroqApi(apiKey);
-
-        List<GroqApi.Message> memory = new ArrayList<>();
-        memory.add(new GroqApi.Message(
-            "system",
+        GroqChatBot bot = new GroqChatBot(
+            api,
+            model,
             "Você é um assistentente que fala português, fale o máximo possível como um ser humano de forma humanizada!"
-        ));
+        );
 
         Scanner sc = new Scanner(System.in);
-
-        String model = "llama-3.3-70b-versatile";
 
         System.out.println("Bem vindo, você está conversando com o modelo: "+model);
         System.out.println("Escreva \"sair\" para encerrar!");
@@ -37,24 +33,11 @@ public class ChatBotExample {
                 break;
             }
 
-            memory.add(new GroqApi.Message(
-                "user",
-                userMessage
-            ));
-
-            String assistantMessage = api.completions(
-                model,
-                memory
-            );
+            String assistantMessage = bot.completions(userMessage);
 
             System.out.println();
             System.out.println(assistantMessage);
             System.out.println();
-
-            memory.add(new GroqApi.Message(
-                "assistant",
-                assistantMessage
-            ));
 
         }
 
